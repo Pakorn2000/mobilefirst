4542260f5c9a22a0dd3991f051f35d403c7f4235
Pakorn2000 <s6202041620088@email.kmutnb.ac.th>
2021-09-28 17:11:51 +0700

    เพิ่ม Call api

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobilefirst/model/activity.dart';
+
+var data;
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -9,9 +13,69 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   @override
+  void initState() {
+    super.initState();
+    callActivity();
+  }
+
+  Future<void> callActivity() async {
+    print("hello world");
+
+    // กำหนด Path
+    var url = Uri.parse("https://www.boredapi.com/api/activity");
+
+    // call api
+    var response = await http.get(url);
+    // การสั่งให้แอปวาดหน้าจอใหม่ โดยการใช้ funtion setState
+
+    setState(() {
+      // แปลงจาก json => object
+      data = cedFromJson(response.body);
+
+      // การเข้าถึงข้อมูลใน object
+      print(data.activity);
+      print(data.type);
+      print(data.participants);
+      print(data.price);
+      print(data.link);
+      print(data.key);
+      print(data.accessibility);
+    });
+
+    // แสดงสถานะของcode
+    print(response.statusCode);
+
+    // แสดงข้อมูล
+    print(response.body);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Dashboard'),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(Icons.api_sharp),
+              SizedBox(
+                width: 10,
+              ),
+              Text('DashBoard'),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            Text(
+              data?.activity ?? 'กำลังโหลดข้อมูล',
+              ),
+            Text(data?.type ?? ''),
+            Text('${data?.participants ?? ""}'),
+            Text('${data?.price ?? ""}'),
+          ],
+        ),
+      ),
     );
   }
 }
